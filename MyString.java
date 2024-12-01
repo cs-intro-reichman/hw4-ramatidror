40112f5962dea06da37738c0328e7fd7d9d68d28
Dror Ramati <dror.ramati@post.runi.ac.il>
2024-12-01 20:04:38 +0200

    Update MyString.java

--- MyString.java
+++ MyString.java
@@ -15,8 +15,7 @@ public class MyString {
         System.out.println(contains("personality", "dad")); // false
         System.out.println(contains("resignation", "sign")); // true
     }
-
-
+    
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
         int lenth = str.length();
@@ -32,26 +31,32 @@ public class MyString {
         }
         return newStr;
     }
-
+    
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-    str1 = lowerCase(str1);
-    str2 = lowerCase(str2);
+        
     int increaser = 0;
-    char checkChar = str2.charAt(0);
-
+    
     if (str2.length() > str1.length()) {
             return false;
         }
-    else if (str1.indexOf(checkChar) == -1) {
-        return false;
 
-    } else {
+    else if (str2.length() == 0) {
+        return true;
+    }
+
+    char checkChar = str2.charAt(0);
+
+    if (str1.indexOf(checkChar) == -1) {
+        return false;
+    }
+    else {
              for (int i =  str1.indexOf (checkChar); i < (str2.length() + str1.indexOf (checkChar)); i++, increaser++) {
-                  if (str2.charAt(increaser) != str1.charAt(i)) {
+
+                 if (str2.charAt(increaser) != str1.charAt(i)) {
                       return false;
             }
-             }
+         }
         return  true;
          }
     }
