5a527d11a0eff0a5f0baf8987b28004195fa1dc8
Dror Ramati <dror.ramati@post.runi.ac.il>
2024-12-01 16:54:50 +0200

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -1,5 +1,41 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+int givvenNumber = Integer.parseInt(args [0]);
+        System.out.println ("Prime numbers up to " + givvenNumber);
+
+boolean [] isPrime = new boolean [givvenNumber + 1];
+
+    for (int i = 0; i <= givvenNumber; i++) {
+        isPrime [i] = true;
+        }
+
+        isPrime [0] = false;
+        isPrime [1] = false;
+
+        for (int p = 2; p*p <= givvenNumber; p ++) {
+
+            if (isPrime[p]) {
+
+                for (int i = p * p; i <= givvenNumber; i += p) {
+                    isPrime[i] = false;
+                }
+            }
+        }
+        int counter = 0;
+        for (int i = 0; i <= givvenNumber; i ++) {
+            if (isPrime[i]) {
+                counter++;
+            }
+        }
+
+
+        for (int i = 0; i <= givvenNumber; i ++) {
+            if (isPrime[i]) {
+                System.out.println (i);
+            }
+        }
+        int precent = (counter * 100) / givvenNumber;
+        System.out.println("There are " + counter + " primes between 2 and " + givvenNumber + " (" + precent + "% are primes)");
+
     }
 }
