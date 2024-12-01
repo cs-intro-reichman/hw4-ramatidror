8a204a0209c1ea3835f99fe7588d26784145a605
Dror Ramati <dror.ramati@post.runi.ac.il>
2024-12-01 18:06:20 +0200

    Update MyString.java

--- MyString.java
+++ MyString.java
@@ -16,15 +16,43 @@ public class MyString {
         System.out.println(contains("resignation", "sign")); // true
     }
 
+
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        int lenth = str.length();
+        String newStr = "";
+        for (int i = 0; i < lenth; i++) {
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+                newStr = newStr + (char) (str.charAt(i) + 32);
+            } else if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+                newStr = newStr + (str.charAt(i));
+            } else {
+                newStr = newStr + (str.charAt(i));
+            }
+        }
+        return newStr;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+    str1 = lowerCase(str1);
+    str2 = lowerCase(str2);
+    int increaser = 0;
+    char checkChar = str2.charAt(0);
+
+    if (str2.length() > str1.length()) {
+            return false;
+        }
+    else if (str1.indexOf(checkChar) == -1) {
         return false;
+
+    } else {
+             for (int i =  str1.indexOf (checkChar); i < (str2.length() + str1.indexOf (checkChar)); i++, increaser++) {
+                  if (str2.charAt(increaser) != str1.charAt(i)) {
+                      return false;
+            }
+             }
+        return  true;
+         }
     }
 }
